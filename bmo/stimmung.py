a15ba07f6ca0f35e2ddc8182930735f019729e90
ayoub <ayoub.kantari@alumni.fh-aachen.de>
2026-04-21 20:10:55 +0200

    stuimmung.py wurd implemntiert

--- bmo/stimmung.py
+++ bmo/stimmung.py
@@ -1,1 +1,22 @@
-# Zustandsmaschine (idle/happy/denkt/spricht)
+from enum import Enum
+
+
+class Zustand(Enum):
+    IDLE = 1
+    DENKT = 2
+    SPRICHT = 3
+
+
+class Stimmung:
+
+    def __init__(self):
+        self.aktueller_zustand = Zustand.IDLE
+
+    def wechsle(self, nächster):
+        self.aktueller_zustand = nächster
+
+    def unterbrechen(self):
+        self.aktueller_zustand = Zustand.IDLE
+
+    def get_zustand(self):
+        return self.aktueller_zustand
