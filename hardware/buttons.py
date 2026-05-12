7b22617b432e5f0999784271faabeeac86c4a89f
ayoub <ayoub.kantari@alumni.fh-aachen.de>
2026-05-12 18:18:13 +0200

    hardware/buttons.py: PUD_DOWN und bouncetime fuer GPIO Edge Detection

--- hardware/buttons.py
+++ hardware/buttons.py
@@ -12,11 +12,11 @@ BUTTON_B = 27
 def setup(stimmung):
     GPIO.setmode(GPIO.BCM)
 
-    GPIO.setup(BUTTON_A, GPIO.IN)
-    GPIO.setup(BUTTON_B, GPIO.IN)
+    GPIO.setup(BUTTON_A, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+    GPIO.setup(BUTTON_B, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 
-    GPIO.add_event_detect(BUTTON_A, GPIO.RISING, callback=lambda x: stimmung.wechsle(Zustand.DENKT))
-    GPIO.add_event_detect(BUTTON_B, GPIO.RISING, callback=lambda x: stimmung.unterbrechen())
+    GPIO.add_event_detect(BUTTON_A, GPIO.RISING, callback=lambda x: stimmung.wechsle(Zustand.DENKT), bouncetime=200)
+    GPIO.add_event_detect(BUTTON_B, GPIO.RISING, callback=lambda x: stimmung.unterbrechen(), bouncetime=200)
 
 
 def cleanup():
